e627f59e09ee6898604f22cba20472c89da8e796
Yousef Mohamed <yousefmohamed17479191@gmail.com>
2025-10-28 17:21:10 +0300

    add binding

--- lib/core/utils/binding.dart
+++ lib/core/utils/binding.dart
@@ -1,10 +1,12 @@
 
 import 'package:ecommerce_supbase/features/onboarding/presentation/view_model/onboarding_view_model.dart';
+import 'package:ecommerce_supbase/features/settings/presentation/view_model/theme_controller.dart';
 import 'package:get/get.dart';
 
 class Binding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => OnBoardingViewModel());
+    Get.lazyPut(() => ThemeController());
   }
 }
